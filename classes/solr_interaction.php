f670db0273024c5da6f278ad1b785d8152c39b88
Hartmut Beyer <beyer@hab.de>
2022-02-16 13:46:38 +0100

    ESM hinzugefügt

--- classes/solr_interaction.php
+++ classes/solr_interaction.php
@@ -29,7 +29,7 @@ class solr_interaction {
         'all' => 'Alle',
         '141678615' => 'Antoinette Amalie von Braunschweig-Wolfenbüttel',
         '128989289' => 'Bahnsen, Benedikt',
-        '104277122' => 'Elisabeth Sophie Marie von  Braunschweig-Lüneburg'
+        '104277122' => 'Elisabeth Sophie Marie von  Braunschweig-Lüneburg',
         '117364177' => 'Sturm, Leonhard Christoph',
         '11566-6' => 'Ausleihverzeichnisse der HAB',
         '116118547' => 'Caselius, Johannes',
